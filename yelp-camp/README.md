7f9090f4414b3fd12509c8e28c3b61ca95853b7c
sunilzs <sunylkumar18@gmail.com>
2016-08-02 02:43:06 -0400

    YelpCamp Campgrounds and Add Campgrounds page

--- yelp-camp/README.md
+++ yelp-camp/README.md
@@ -1,1 +1,27 @@
-YelpCamp App!
+#YelpCamp App!
+
+*Add Landing page
+*Add Campground page that lists all campgrounds
+
+Each Campground has:
+*Name
+*Image
+
+[
+    {name: "Stone Camp", image: "http://www.image.com"},
+]
+
+
+#Create New Campgrounds
+*Setup new campground post route
+*Add in body parser
+*Set up route to show form
+*Add basic unstyled form
+
+#Style the campgrounds page
+*Add a better header/title
+*Make campgrounds display in a grid
+
+#Style Navbar and Form
+*Add a navbar to all templates
+*Style the naw campgrounds form
---
+++ yelp-camp/v1/server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+
+var campgrounds = [
+    { campName: "Stone Camps", campImage: "https://farm8.staticflickr.com/7301/16360364188_863a4c83b1.jpg" },
+     { campName: "Rock Camps", campImage: "https://farm7.staticflickr.com/6008/6019595072_13abe8df82.jpg" },
+     { campName: "Sky Camps", campImage: "https://farm8.staticflickr.com/7329/12635844085_57b2cd7006.jpg" },
+     { campName: "Mountain Camps", campImage: "https://farm3.staticflickr.com/2825/8990714887_394f283f95.jpg" },
+]
+
+
+app.get('/', function (req, res) {
+    res.render('landing');
+});
+
+app.get('/campgrounds', function (req, res) {
+    res.render('campgrounds', { campgrounds: campgrounds });
+})
+
+app.get('/campgrounds/new', function (req, res) {
+    res.render('newcampground', { campgrounds: campgrounds });
+})
+
+app.post('/campgrounds', function (req, res) {
+    var newCampground = req.body;
+    campgrounds.push(newCampground)
+    res.redirect("/campgrounds");
+})
+
+
+app.listen(PORT, function () {
+    console.log("listening on " + PORT);
+}); 
